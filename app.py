7d6854a672f86e7faf3486c26d0c317c0546fe53
John Crawford <crawfordjohn@ufl.edu>
2024-04-18 14:35:31 -0400

    display shortest path on Flask

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import geopandas as gpd
 import folium
 import io
 import pandas as pd
+import networkx as nx
+from folium.plugins import MarkerCluster
 
 
 app = Flask(__name__)
@@ -12,32 +14,58 @@ app = Flask(__name__)
 
 @app.route("/")
 def fullscreen():
-    # Coordinates for Florida
-    lat_min = 24.396308
-    lat_max = 27.001056
-    lon_min = -82.634938
-    lon_max = -78.974306
-
-    # Creates a map centered around Florida
-    m = folium.Map(location=[(lat_min + lat_max) / 2, (lon_min + lon_max) / 2], zoom_start=6)
-
-    # Restricts map view to Florida
-    m.fit_bounds([[lat_min, lon_min], [lat_max, lon_max]])
-
-    # Load nodes from CSV file
+    ##Load the Data
     nodes_df = pd.read_csv('nodes.csv')
+    edges_df = pd.read_csv('edges.csv').reset_index()[['u', 'v', 'length']]
 
-    # Sample a subset of nodes (e.g., 100 nodes)
-    sample_size = 100
-    sampled_nodes = nodes_df.sample(n=sample_size)
+    # Create Graph and Initialize start and end node
+    G = nx.from_pandas_edgelist(edges_df, 'u', 'v', edge_attr=True, create_using=nx.Graph())
+    start_node, end_node = nodes_df['osmid'].sample(2, random_state=0)
 
-    # Add each sampled node as a dot on the map
-    for index, row in sampled_nodes.iterrows():
-        folium.Marker(location=[row['y'], row['x']], popup=str(row['osmid'])).add_to(m)
+    ##Need to implement these from stratch
+    path = nx.dijkstra_path(G, start_node, end_node, weight='length')
+    print(nx.shortest_path_length(G, start_node, end_node, weight='length'))
+
+    # get data for nodes and edges along shortest path
+    selected_nodes = nodes_df[nodes_df['osmid'].isin(path)]
+    selected_edges = edges_df[
+        (edges_df['u'].isin(selected_nodes['osmid'])) & (edges_df['v'].isin(selected_nodes['osmid']))]
+
+    # create map
+    m = folium.Map(location=[selected_nodes['y'].mean(), selected_nodes['x'].mean()], zoom_start=13)
+    marker_cluster = MarkerCluster().add_to(m)
+
+    # add the nodes to the map
+    for _, row in selected_nodes.iterrows():
+        if row['osmid'] in [start_node, end_node]:
+            folium.Marker(
+                location=[row['y'], row['x']],
+                popup=f"id: {row['osmid']}, lat: {row['y']}, lon: {row['x']}",
+                icon=folium.Icon(color='blue', icon='map-marker')
+            ).add_to(m)
+        # uncomment if want to show intermediate nodes
+        """else:
+            folium.Marker(
+                location=[row['y'], row['x']],
+                popup=f"id: {row['osmid']}, lat: {row['lat']}, lon: {row['lon']}",
+                icon=folium.Icon(color='blue', icon='map-marker')
+            ).add_to(m)
+            """
+
+    # add the edges to the map
+    for _, row in selected_edges.iterrows():
+        folium.PolyLine(
+            locations=[(nodes_df.loc[nodes_df['osmid'] == row['u']]['y'].values[0],
+                        nodes_df.loc[nodes_df['osmid'] == row['u']]['x'].values[0]),
+                       (nodes_df.loc[nodes_df['osmid'] == row['v']]['y'].values[0],
+                        nodes_df.loc[nodes_df['osmid'] == row['v']]['x'].values[0])],
+            popup=f"start_node_id: {row['u']}, end_node_id: {row['v']}, dist: {row['length']}",
+            color='red',
+            weight=2,
+            opacity=0.5
+        ).add_to(m)
 
     return m._repr_html_()
-
-
 
 
 @app.route("/iframe")
